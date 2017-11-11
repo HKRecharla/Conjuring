4c53877039a7146cbaae5c5dfc3d0d5600fdef5f
tech <harikrishna.recharla@arvindinternet.com>
2017-11-11 22:06:50 +0530

    message

---
+++ Dee/src/gopalpack/GopalSample.java
@@ -0,0 +1,14 @@
+package gopalpack;
+/**
+@author Harikrishna Recharla SDET II
+@date 11-Nov-2017 9:57:43 PM
+*/
+public class GopalSample {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("jhgfbjm");
+	}
+	
+}
---
+++ Dee/src/haripack/HariSmaple.java
@@ -0,0 +1,8 @@
+package haripack;
+/**
+@author Harikrishna Recharla SDET II
+@date 11-Nov-2017 9:59:48 PM
+*/
+public class HariSmaple {
+
+}
