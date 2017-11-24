762afc7c19d867bd1dff8b7e0c5d02d70a732b79
deeptha <deeptha@arvindinternet.com>
2017-11-24 23:21:04 +0530

    swap number without thrid variable

---
+++ Dee/src/sam/SwapNumberWithoutThridvar.java
@@ -0,0 +1,21 @@
+package sam;
+
+import java.util.Scanner;
+
+public class SwapNumberWithoutThridvar 
+{
+public static void main(String args[])
+{
+	Scanner sc = new Scanner(System.in);
+	
+	int num1 = sc.nextInt();
+	int num2 = sc.nextInt();
+	
+	System.out.println("Number before swap " +   num1  +  num2);
+	
+	num1 = num1 + num2;
+	num2 = num1 - num2;
+	num1 = num1 - num2; 
+	System.out.println("Number after swap " +   num1  +  num2);
+}
+}
