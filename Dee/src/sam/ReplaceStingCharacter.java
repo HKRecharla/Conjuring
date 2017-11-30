df060cff3b1dda526607b0a4c2386b37c0762a3b
deeptha <deeptha@arvindinternet.com>
2017-11-30 22:41:48 +0530

    Replace string Character

---
+++ Dee/src/sam/ReplaceStingCharacter.java
@@ -0,0 +1,24 @@
+package sam;
+
+import java.util.Scanner;
+
+public class ReplaceStingCharacter 
+{
+	public static void main(String[] args){
+		System.out.println("Enter String");
+		
+		String str = "ArvindInternetLimited is nice company";
+		
+		String replaceString = str.replace("is","was");
+		
+		System.out.println(replaceString);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
